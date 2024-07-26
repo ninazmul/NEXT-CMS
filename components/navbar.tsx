f7964e2c13eb5f4b3bb108a1681068c1f0b6c654
ninazmul <nazmulsaw@gmail.com>
2024-07-26 23:10:08 +0600

    Theme changing functionality done

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -5,6 +5,7 @@ import StoreSwitcher from "@/components/store-switcher";
 import { auth } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 import prismadb from "@/lib/prismadb";
+import { ThemeToggle } from "./theme-toggle";
 
 export default async function Navbar() {
   const { userId } = auth();
@@ -25,6 +26,7 @@ export default async function Navbar() {
         <StoreSwitcher items={stores} />
         <MainNav className="mx-6" />
         <div className="ml-auto flex items-center space-x-4">
+          <ThemeToggle />
           <UserButton afterSignOutUrl="/" />
         </div>
       </div>
